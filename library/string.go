f1a69eee1927b422e7208c45bf566eeb1ba90da3
chenlipeng <850391612@qq.com>
2020-10-13 19:27:19 +0800

    UuidGenerator

--- library/string.go
+++ library/string.go
@@ -2,6 +2,7 @@ package library
 
 import (
 	"crypto/md5"
+        "crypto/rand"
 	"fmt"
 	"math/rand"
 	"time"
@@ -22,4 +23,14 @@ func Md5(str string) string {
 	has := md5.Sum(data)
 	md5str := fmt.Sprintf("%x", has)
 	return md5str
+}
+
+func UuidGenerator() (uuid string) {
+	uuid = ""
+        b := make([]byte, 16)
+        _, err := rand.Read(b)
+        if err == nil {
+		uuid = fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+        }
+        return uuid
 }
